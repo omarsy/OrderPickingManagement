11189dbea4ff564274406900e9b69f67c81bd9c0
majdaelkhorji <majdaelkhorji@gmail.com>
2018-03-12 16:07:51 +0100

    update

--- web-app/src/data/lieuPicking.js
+++ web-app/src/data/lieuPicking.js
@@ -4,11 +4,16 @@ const lieuPicking = {
         //int
         distance: 22
     },
-    "allee/1/section/2/vers/allee/2/section/1":{
+    "allee/1/section/2/vers/allee/2/section/2":{
         //int
         distance: 22
     },
-    "allee/2/section/1/vers/allee/1/section/2":{
+    
+    "allee/2/section/1/vers/allee/3/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/2/section/2/vers/allee/3/section/2":{
         //int
         distance: 10
     },
@@ -17,67 +22,111 @@ const lieuPicking = {
         distance: 10
     },
 
-    "allee/3/section/1/vers/allee/1/section/2":{
+    "allee/3/section/1/vers/allee/4/section/1":{
         //int
         distance: 12
     },
-    "allee/3/section/2/vers/allee/2/section/1":{
+    "allee/3/section/1/vers/allee/3/section/2":{
         //int
         distance: 12
     },
-    "allee/4/section/1/vers/allee/1/section/2":{
+    "allee/3/section/2/vers/allee/4/section/2":{
+        //int
+        distance: 12
+    },
+    "allee/4/section/1/vers/allee/5/section/1":{
         //int
         distance: 17
     },
-    "allee/4/section/2/vers/allee/2/section/1":{
+    "allee/4/section/1/vers/allee/4/section/2":{
         //int
         distance: 17
     },
-    "allee/5/section/1/vers/allee/1/section/2":{
+    "allee/4/section/2/vers/allee/5/section/2":{
+        //int
+        distance: 17
+    },
+    "allee/5/section/1/vers/allee/6/section/1":{
         //int
         distance: 18
     },
-    "allee/5/section/2/vers/allee/2/section/1":{
+    "allee/5/section/2/vers/allee/6/section/1":{
         //int
         distance: 10
     },
-    "allee/6/section/1/vers/allee/1/section/2":{
+    "allee/5/section/1/vers/allee/6/section/2":{
+        //int
+        distance: 10
+    },
+    "allee/6/section/1/vers/allee/7/section/1":{
         //int
         distance: 13
     },
-    "allee/6/section/2/vers/allee/2/section/1":{
+    "allee/6/section/1/vers/allee/7/section/2":{
         //int
         distance: 10
     },
-    "allee/7/section/1/vers/allee/1/section/2":{
+    "allee/6/section/2/vers/allee/7/section/2":{
+        //int
+        distance: 10
+    },
+    "allee/7/section/1/vers/allee/8/section/1":{
         //int
         distance: 16
     },
-    "allee/7/section/2/vers/allee/2/section/1":{
+    "allee/7/section/1/vers/allee/8/section/2":{
         //int
         distance: 10
     },
-    "allee/8/section/1/vers/allee/1/section/2":{
+    "allee/7/section/2/vers/allee/8/section/2":{
+        //int
+        distance: 10
+    },
+    "allee/8/section/1/vers/allee/9/section/1":{
         //int
         distance: 47
     },
-    "allee/8/section/2/vers/allee/2/section/1":{
+    "allee/8/section/1/vers/allee/9/section/2":{
+        //int
+        distance: 47
+    },
+    "allee/8/section/2/vers/allee/9/section/2":{
         //int
         distance: 10
     },
-    "allee/9/section/1/vers/allee/1/section/2":{
+    "allee/9/section/1/vers/allee/10/section/1":{
         //int
         distance: 18
     },
-    "allee/9/section/2/vers/allee/2/section/1":{
+    "allee/9/section/1/vers/allee/10/section/2":{
+        //int
+        distance: 18
+    },
+    "allee/9/section/2/vers/allee/10/section/2":{
         //int
         distance: 10
     },
-    "allee/10/section/1/vers/allee/1/section/2":{
+    "allee/9/section/2/vers/allee/10/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/9/section/2/vers/allee/10/section/2":{
+        //int
+        distance: 18
+    },
+    "allee/10/section/1/vers/allee//section/2":{
         //int
         distance: 25
     },
     "allee/10/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/10/section/2/vers/allee/2/section/2":{
+        //int
+        distance: 10
+    },
+    "allee/10/section/1/vers/allee/2/section/1":{
         //int
         distance: 10
     },
