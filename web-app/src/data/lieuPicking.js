0ae9e433ed9ab18668f17baa8917104dde0c0fc6
majdaelkhorji <majdaelkhorji@gmail.com>
2018-03-12 15:16:55 +0100

    update

--- web-app/src/data/lieuPicking.js
+++ web-app/src/data/lieuPicking.js
@@ -1,45 +1,85 @@
 const lieuPicking = {
 
-    "allee/1/section/1":{
+    "allee/1/section/1/vers/allee/1/section/2":{
         //int
         distance: 22
     },
-
-    "allee/2/section/2":{
+    "allee/1/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 22
+    },
+    "allee/2/section/1/vers/allee/1/section/2":{
         //int
         distance: 10
     },
-    "allee/3/section/3":{
+    "allee/2/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+
+    "allee/3/section/1/vers/allee/1/section/2":{
         //int
         distance: 12
     },
-    "allee/4/section/4":{
+    "allee/3/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 12
+    },
+    "allee/4/section/1/vers/allee/1/section/2":{
         //int
         distance: 17
     },
-    "allee/5/section/5":{
+    "allee/4/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 17
+    },
+    "allee/5/section/1/vers/allee/1/section/2":{
         //int
         distance: 18
     },
-    "allee/6/section/6":{
+    "allee/5/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/6/section/1/vers/allee/1/section/2":{
         //int
         distance: 13
     },
-    "allee/7/section/7":{
+    "allee/6/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/7/section/1/vers/allee/1/section/2":{
         //int
         distance: 16
     },
-    "allee/8/section/8":{
+    "allee/7/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/8/section/1/vers/allee/1/section/2":{
         //int
         distance: 47
     },
-    "allee/9/section/9":{
+    "allee/8/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/9/section/1/vers/allee/1/section/2":{
         //int
         distance: 18
     },
-    "allee/10/section/10":{
+    "allee/9/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
+    "allee/10/section/1/vers/allee/1/section/2":{
         //int
         distance: 25
     },
+    "allee/10/section/2/vers/allee/2/section/1":{
+        //int
+        distance: 10
+    },
 }
- export default lieuPicking;
+
